4568e99736eba4825b8183e3cb04dbc5b14511ff
MateuszM02 <96058912+MateuszM02@users.noreply.github.com>
2023-12-30 13:44:54 +0100

    Create Prezydent.java

---
+++ PO/listy/lista5/zad1/Prezydent.java
@@ -0,0 +1,6 @@
+public class Prezydent extends Rzadzacy
+{
+    public Prezydent(){super();}
+    public Prezydent(String i, String naz, String nar){super(i,naz,nar);}
+    public int ranga()  {return 3;}
+}
