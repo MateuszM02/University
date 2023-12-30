df8feb5f26d84fd874009615f0ec7ce3cb334b7c
MateuszM02 <96058912+MateuszM02@users.noreply.github.com>
2023-12-30 13:44:36 +0100

    Create Premier.java

---
+++ PO/listy/lista5/zad1/Premier.java
@@ -0,0 +1,6 @@
+public class Premier extends Rzadzacy
+{
+    public Premier(){super();}
+    public Premier(String i, String naz, String nar){super(i,naz,nar);}
+    public int ranga()  {return 4;}
+}
