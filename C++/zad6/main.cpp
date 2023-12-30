cf792f651258f4069f4884630d2fc82531000952
MateuszM02 <96058912+MateuszM02@users.noreply.github.com>
2023-12-30 12:28:01 +0100

    Create main.cpp

---
+++ C++/zad6/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "wyrazenie.hpp"
+#include "liczba.hpp"
+#include "stala.hpp"
+#include "zmienne.hpp"
+
+using namespace std;
+
+int main()
+{
+    liczba x = liczba(3);
+    //liczba y = liczba(5);
+    //cout<<x.GetValue()<<" , "<<y.GetValue()<<endl;
+    return 0;
+}
