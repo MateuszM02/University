f917e48eb6e92881575a7ace6a4ec304f1ef31ab
MateuszM02 <96058912+MateuszM02@users.noreply.github.com>
2023-12-30 13:44:01 +0100

    Create Hierarchia.java

---
+++ PO/listy/lista5/zad1/Hierarchia.java
@@ -0,0 +1,5 @@
+public interface Hierarchia
+{
+    int ranga(); //im nizszy numer tym wieksza ranga
+    boolean czy_wazniejszy_od(Rzadzacy r); //zwraca true, jesli aktualna osoba jest wazniejsza od osoby r, lub false w p.p.
+}
