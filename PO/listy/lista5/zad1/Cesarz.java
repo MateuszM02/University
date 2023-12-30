e432b3a0f810ce70a2c5380e1c2d4823aca157e0
MateuszM02 <96058912+MateuszM02@users.noreply.github.com>
2023-12-30 13:43:10 +0100

    Create Cesarz.java

---
+++ PO/listy/lista5/zad1/Cesarz.java
@@ -0,0 +1,6 @@
+public class Cesarz extends Rzadzacy
+{
+    public Cesarz() {super();}
+    public Cesarz(String i, String naz, String nar) {super(i,naz,nar);}
+    public int ranga()  {return 1;}
+}
