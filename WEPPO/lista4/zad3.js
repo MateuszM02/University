21b920902cd4509d3b954e536259f4360b2eb8d4
MateuszM02 <96058912+MateuszM02@users.noreply.github.com>
2024-01-14 17:24:35 +0100

    Create zad3.js

---
+++ WEPPO/lista4/zad3.js
@@ -0,0 +1,7 @@
+let f1 = require('./zad3_fun1')
+let f2 = require('./zad3_fun2')
+
+console.log( f1.mult(4, 4) ) //4 * 4 = 16
+console.log( f2.add(9, 10) ) //9 + 10 = 19
+console.log( f2.multadd(2, 3, 4) ) //2 * ( 3 + 4 ) = 2 * 7 = 14 //f1 i f2 musza miec cykl, zeby zadzialalo
+f1.printadd(2, 3) //2 + 3 = 5
